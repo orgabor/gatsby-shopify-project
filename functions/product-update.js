3b6d25a857d0639038899c6f52111c40293ae9ee
Orgabor <gabor.gazdag@orgabor.com>
2020-11-24 16:38:04 +0100

    product update function

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,6 +1,27 @@
-export.handler = function(event, context, callback){
-	callback(null,{
-		statusCode: 200,
-		body: 'Hello World!',
-	})
-}
+const faunadb = require('faunadb');
+const verifyWebhookIntegrity = require('shopify-verify-webhook')
+
+const q = faunadb.query;
+
+const client = new faunadb.Client({
+	secret: process.env.FAUNADB_CLIENT,
+})
+
+exports.handler = function(event, context, callback){
+
+	const isValid = verifyWebhookIntegrity(process.env.SHOPIFYWEBHOOK_KEY,
+		event.headers['x-shopify-hmac-sha256'],
+		event.body);
+	if(isValid){
+		callback(null,{
+			statusCode: 200,
+			body: 'Hello World!',
+		});
+	}else{
+		callback(null,{
+			statusCode: 403,
+			body: 'Errore Cazzo',
+		});
+	}
+	
+};
